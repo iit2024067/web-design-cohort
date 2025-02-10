733183b1710daf9a21b6d15344228ed184570f2d
iit2024067 <iit2024067@iiita.ac.in>
2025-02-10 09:13:34 +0530

    Add files via upload

---
+++ iit2024067_task1.js
@@ -0,0 +1,7 @@
+let h1=document.querySelector('h1');
+let p=document.querySelector('p');
+let a=document.querySelector('a');
+
+h1.textContent="Task-1";
+p.textContent="In this task, I use js to fill text on my web page";
+a.textContent="This is anchor tag by js";
